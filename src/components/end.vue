962f990e2e0cf64406c04440b4784c310bf2999c
oobika90 <oobika@gmail.com>
2017-04-21 13:00:38 +0300

    Images

--- src/components/end.vue
+++ src/components/end.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>{{$t("endspeech.3")}}</h1>
     <p>{{$t("endspeech.1")}}</p>
+    <img src="https://www.colourbox.com/preview/2630929-black-and-white-picture-retro-bow-with-a-handkerchief.jpg" alt="Deep bow" style="width:30%;">
     <p>{{$t("endspeech.2")}}</p>
   </div>
 </template>
