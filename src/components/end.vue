73e66726c9c7971e5f1256cfab7ffae622c524f9
oobika90 <oobika@gmail.com>
2017-04-23 18:31:44 +0300

    Survey Code integration

--- src/components/end.vue
+++ src/components/end.vue
@@ -5,6 +5,7 @@
     <img src="https://www.colourbox.com/preview/2630929-black-and-white-picture-retro-bow-with-a-handkerchief.jpg" alt="Deep bow" style="width:30%;">
     <p>{{$t("endspeech.2")}}</p>
     <p>{{$t("share.2")}} <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftaju-ca821.firebaseapp.com%2F&amp;src=sdkpreparse"> {{$t("share.1")}} </a></p>
+    <p><a href="https://www.surveycircle.com/UJK3-TS3J-F731-2EGK" target="_blank"> Redeem Survey Code </a></p>
   </div>
 </template>
 
